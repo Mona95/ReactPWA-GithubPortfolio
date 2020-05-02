d5fe35bbd5d9098adc98bcc3ba724cb9143dea71
Mona95 <shamona9594@gmail.com>
2020-05-02 23:22:28 +0300

    using try/catch for pwa if there wasn't any data to be loaded

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -11,6 +11,7 @@ const ProfileWrapper = styled.div`
 
 const Avatar = styled.img`
   width: 150px;
+  border-radius: 10px;
 `;
 
 export class Profile extends Component {
@@ -18,26 +19,36 @@ export class Profile extends Component {
     data: {},
     repositories: [],
     loading: true,
+    error: "",
   };
 
   componentDidMount() {
-    axios.get("https://api.github.com/users/mona95").then((res) => {
-      if (res.status === 200) {
-        axios.get("https://api.github.com/users/Mona95/repos").then((repo) => {
-          res.status === 200 &&
-            this.setState({
-              data: res.data,
-              repositories: repo.data,
-              loading: false,
+    try {
+      axios.get("https://api.github.com/users/mona95").then((res) => {
+        if (res.status === 200) {
+          axios
+            .get("https://api.github.com/users/Mona95/repos")
+            .then((repo) => {
+              res.status === 200 &&
+                this.setState({
+                  data: res.data,
+                  repositories: repo.data,
+                  loading: false,
+                });
             });
-        });
-      }
-    });
+        }
+      });
+    } catch (error) {
+      this.setState({
+        loading: false,
+        error: error.message,
+      });
+    }
   }
   render() {
-    const { data, loading, repositories } = this.state;
-    if (loading) {
-      return <div>loading...</div>;
+    const { data, loading, repositories, error } = this.state;
+    if (loading || error) {
+      return <div>{loading ? "Loading..." : error}</div>;
     }
 
     const items = [
@@ -60,7 +71,7 @@ export class Profile extends Component {
     console.log(projects);
     return (
       <ProfileWrapper>
-        <Avatar className="Profile-avatar" src={data.avatar_url} alt="avatar" />
+        <Avatar src={data.avatar_url} alt="avatar" />
         <List title="Profile" items={items} />
         <List title="Projects" items={projects} />
       </ProfileWrapper>
